055a4ada3b1b468b07d7f85e6fe169f6eb30e3b4
Collin Laney <collinlaney.m+github@gmail.com>
2024-02-19 02:33:33 -0500

    Create a second script that focuses on exporting the information to a JSON table
    Ultimately not what I want, I still want console output, but I do like the JSON export. I still have to tinker with it a bit.

--- Get-SystemInformation/Get-SystemInformation.ps1
+++ Get-SystemInformation/Get-SystemInformation.ps1
@@ -1,110 +1,155 @@
-function Get-SystemInformation {
-
-    <#
-    .SYNOPSIS
-    Retrieves system information such as operating system details, processor information, memory details, GPU information, and network adapter information.
-
-    .DESCRIPTION
-    This function retrieves various system information based on the detected operating system (Windows or Linux). It displays the system name, operating system details, processor information, memory details, GPU information, and network adapter information. The output can be optionally dumped into a text file.
-
-    .PARAMETER OutputFile
-    Specifies the name of the text file to which the output should be dumped. If this parameter is not provided, the output will be displayed in the console.
-
-    .PARAMETER OperatingSystem
-    Specifies whether to display only operating system information.
-
-    .PARAMETER Processor
-    Specifies whether to display only processor information.
-
-    .PARAMETER Memory
-    Specifies whether to display only memory information.
-
-    .PARAMETER GPU
-    Specifies whether to display only GPU information.
-
-    .PARAMETER NetworkAdapter
-    Specifies whether to display only network adapter information.
-
-    .EXAMPLE
-    Get-SystemInformation -OutputFile "system_info.txt"
-    This command retrieves system information and dumps it into a text file named "system_info.txt".
-
-    .EXAMPLE
-    Get-SystemInformation -Memory
-    This command retrieves only memory information and displays it in the console.
-    #>
-
+function Get-SystemInfo {
     [CmdletBinding()]
-    param (
-        [string]$OutputFile = "",
-        [switch]$OperatingSystem,
+    param(
         [switch]$Processor,
         [switch]$Memory,
-        [switch]$GPU,
-        [switch]$NetworkAdapter
+        [switch]$Graphics,
+        [switch]$Disk,
+        [switch]$Network,
+        [switch]$System
     )
 
-    # Detect the operating system
-    if ($env:OS -like "*Windows*") {
-        $os = "Windows"
-    } elseif ($env:OS -like "*Linux*") {
-        $os = "Linux"
+    # Determine the operating system
+    if ($env:OS -like 'Windows*') {
+        $OperatingSystem = "Windows"
+    } elseif ($IsLinux -eq $true) {
+        $OperatingSystem = "Linux"
     } else {
-        Write-Output "Unable to determine the operating system."
-        exit
+        $OperatingSystem = "Unknown"
     }
 
-    # Get the system name
-    $systemName = hostname
+    # Define an empty hashtable to store system information
+    $SystemInfo = @{}
 
-    # Display system information based on the operating system
-    if ($os -eq "Windows") {
-        $osInfo = Get-WmiObject Win32_OperatingSystem | Select-Object Caption, Version, OSArchitecture, Manufacturer
-        $processorInfo = Get-WmiObject Win32_Processor | Select-Object Name, Manufacturer, MaxClockSpeed, NumberOfCores
-        $memoryInfo = Get-WmiObject Win32_PhysicalMemory | Measure-Object -Property Capacity -Sum | ForEach-Object { "{0:N2} GB" -f ($_.Sum / 1GB) }
-        $gpuInfo = Get-WmiObject Win32_VideoController | Select-Object Name, AdapterRAM, DriverVersion
-        $networkInfo = Get-WmiObject Win32_NetworkAdapterConfiguration | Where-Object { $_.IPEnabled -eq $true } | Select-Object Description, IPAddress
-    } elseif ($os -eq "Linux") {
-        $osInfo = cat /etc/os-release
-        $processorInfo = cat /proc/cpuinfo
-        $memoryInfo = free -h
-        $gpuInfo = lspci | grep -i vga
-        $networkInfo = ip addr show
+    # Function to get BIOS information
+    function Get-BIOSInfo {
+        if ($OperatingSystem -eq "Windows") {
+            $biosInfo = Get-WmiObject -Class Win32_BIOS
+            $SystemInfo.Add("BIOS", @{
+                "Manufacturer" = $biosInfo.Manufacturer
+                "Version" = $biosInfo.SMBIOSBIOSVersion
+                "ReleaseDate" = $biosInfo.ReleaseDate
+            })
+        }
     }
 
-    # Display system name
-    Write-Output "System Name: $systemName`n"
-
-    # Display system information based on specified categories
-    if ($OperatingSystem) {
-        Write-Output "Operating System Information"
-        $osInfo | Format-Table -AutoSize
-    }
-    if ($Processor) {
-        Write-Output "Processor Information"
-        $processorInfo | Format-Table -AutoSize
-    }
-    if ($Memory) {
-        Write-Output "Memory Information"
-        $memoryInfo | Format-Table -AutoSize
-    }
-    if ($GPU) {
-        Write-Output "GPU Information"
-        $gpuInfo | Format-Table -AutoSize
-    }
-    if ($NetworkAdapter) {
-        Write-Output "Network Adapter Information"
-        $networkInfo | Format-Table -AutoSize
+    # Function to get operating system information
+    function Get-OSInfo {
+        if ($OperatingSystem -eq "Windows") {
+            $osInfo = Get-WmiObject -Class Win32_OperatingSystem
+            $SystemInfo.Add("OperatingSystem", @{
+                "Caption" = $osInfo.Caption
+                "Version" = $osInfo.Version
+                "BuildNumber" = $osInfo.BuildNumber
+            })
+        } elseif ($OperatingSystem -eq "Linux") {
+            $SystemInfo.Add("OperatingSystem", @{
+                "Kernel" = "$(uname -s)"
+                "KernelVersion" = "$(uname -r)"
+            })
+        }
     }
 
-    # Dump output to a text file if specified
-    if ($OutputFile -ne "") {
-        Out-File -FilePath $OutputFile -InputObject $("System Name: $systemName`n" +
-                                                      $(if ($OperatingSystem) {"Operating System Information`n" + ($osInfo | Out-String)})
-                                                      $(if ($Processor) {"Processor Information`n" + ($processorInfo | Out-String)})
-                                                      $(if ($Memory) {"Memory Information`n" + ($memoryInfo | Out-String)})
-                                                      $(if ($GPU) {"GPU Information`n" + ($gpuInfo | Out-String)})
-                                                      $(if ($NetworkAdapter) {"Network Adapter Information`n" + ($networkInfo | Out-String)}))
-        Write-Output "Output has been dumped to $OutputFile"
+    # Function to get processor information
+    function Get-ProcessorInfo {
+        if ($OperatingSystem -eq "Windows") {
+            $processorInfo = Get-WmiObject -Class Win32_Processor
+            $SystemInfo.Add("Processor", @{
+                "Name" = $processorInfo.Name
+                "NumberOfCores" = $processorInfo.NumberOfCores
+                "MaxClockSpeed(GHz)" = "{0:N2}" -f ($processorInfo.MaxClockSpeed / 1GB)
+            })
+        } elseif ($OperatingSystem -eq "Linux") {
+            $processorInfo = $(grep 'model name' /proc/cpuinfo | sort -u | awk -F': ' '{print $2}')
+            $SystemInfo.Add("Processor", @{
+                "Name" = $processorInfo
+            })
+        }
     }
+
+    # Function to get memory information
+    function Get-MemoryInfo {
+        if ($OperatingSystem -eq "Windows") {
+            $memoryInfo = Get-WmiObject -Class Win32_PhysicalMemory
+            $TotalMemoryGB = ($memoryInfo | Measure-Object -Property Capacity -Sum).Sum / 1GB
+            $SystemInfo.Add("Memory", @{
+                "TotalMemory(GB)" = "{0:N2}" -f $TotalMemoryGB
+            })
+        } elseif ($OperatingSystem -eq "Linux") {
+            $memoryInfo = $(grep MemTotal /proc/meminfo | awk '{print $2}')
+            $TotalMemoryGB = $memoryInfo / 1024 / 1024
+            $SystemInfo.Add("Memory", @{
+                "TotalMemory(GB)" = "{0:N2}" -f $TotalMemoryGB
+            })
+        }
+    }
+
+    function Get-GraphicsInfo {
+        if ($OperatingSystem -eq "Windows") {
+            $graphicsInfo = Get-WmiObject -Class Win32_VideoController
+            $GraphicsList = @()
+            foreach ($graphics in $graphicsInfo) {
+                $GraphicsList += @{
+                    "Name" = $graphics.Name
+                    "AdapterRAM(MB)" = "{0:N2}" -f ($graphics.AdapterRAM / 1MB)
+                    "DriverVersion" = $graphics.DriverVersion
+                }
+            }
+            $SystemInfo.Add("Graphics", $GraphicsList)
+        }
+    }
+    
+    function Get-DiskInfo {
+        if ($OperatingSystem -eq "Windows") {
+            $diskInfo = Get-WmiObject -Class Win32_DiskDrive
+            $DiskList = @()
+            foreach ($disk in $diskInfo) {
+                $DiskList += @{
+                    "Model" = $disk.Model
+                    "Size(GB)" = "{0:N2}" -f ($disk.Size / 1GB)
+                    "InterfaceType" = $disk.InterfaceType
+                }
+            }
+            $SystemInfo.Add("Disk", $DiskList)
+        }
+    }
+    
+    function Get-NetworkInfo {
+        if ($OperatingSystem -eq "Windows") {
+            $networkInfo = Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Where-Object { $_.IPEnabled }
+            $NetworkList = @()
+            foreach ($network in $networkInfo) {
+                $NetworkList += @{
+                    "Adapter" = $network.Description
+                    "IPAddress" = $network.IPAddress -join ","
+                    "MACAddress" = $network.MACAddress
+                }
+            }
+            $SystemInfo.Add("Network", $NetworkList)
+        }
+    }    
+
+    # Call functions based on specified parameters
+    if ($System) { Get-OSInfo }
+    if ($Processor) { Get-ProcessorInfo }
+    if ($Memory) { Get-MemoryInfo }
+    if ($Graphics) { Get-GraphicsInfo }
+    if ($Disk) { Get-DiskInfo }
+    if ($Network) { Get-NetworkInfo }
+    if (-not ($System -or $Processor -or $Memory -or $Graphics -or $Disk -or $Network)) {
+        Get-BIOSInfo
+        Get-OSInfo
+        Get-ProcessorInfo
+        Get-MemoryInfo
+        Get-GraphicsInfo
+        Get-DiskInfo
+        Get-NetworkInfo
+    }
+
+    # Generate the filename with current date and time
+    $DateTime = Get-Date -Format "yyyy-MM-dd-HH-mm-ss"
+    $FileName = "$DateTime.json"
+
+    # Export system information to JSON file
+    $SystemInfo | ConvertTo-Json | Out-File -FilePath $FileName
 }
